66f7961c78d9fe406ed49adc941b12938c7be67e
Ederlino de Almeida Camargos <ederlinoacamargos05@gmail.com>
2022-01-10 22:14:03 -0300

    48 - Arquivos Manager

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 });
 
 Route::group(['middleware' => 'access.control.list'], function() {
-    
+
     Route::resource('threads', 'ThreadController');
 });
 
@@ -28,5 +28,18 @@ Route::group(['middleware' => 'access.control.list'], function() {
 Route::post('/replies/store', 'ReplyController@store')->name('replies.store');
 
 Auth::routes();
+
+Route::group(['middleware' => 'auth', 'namespace' => 'Manager', 'prefix' => 'manager'], function(){
+	Route::get('/', function(){
+		return redirect()->route('users.index');
+	});
+
+	Route::resource('roles', 'RoleController');
+	Route::get('roles/{role}/resources', 'RoleController@syncResources')->name('roles.resources');
+	Route::put('roles/{role}/resources', 'RoleController@updateSyncResources')->name('roles.resources.update');
+
+	Route::resource('users', 'UserController');
+	Route::resource('resources', 'ResourceController');
+});
 
 // Route::get('/home', 'HomeController@index')->name('home');
